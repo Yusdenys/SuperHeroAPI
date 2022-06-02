47df81d75ad2b2c3d5ad357ab9eb764de39996a6
yusdenys <yudemendoza@gmail.com>
2022-06-02 12:28:29 -0400

    First commit

--- SuperHeroAPI/Controllers/SuperHeroController.cs
+++ SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -15,12 +15,60 @@ namespace SuperHeroAPI.Controllers
                     FirstName = "Peter",
                     LastName = "Parker",
                     Place = "New York City"
+                },
+                new SuperHero {
+                    Id = 2,
+                    Name = "IronMan",
+                    FirstName = "Tony",
+                    LastName = "Stark",
+                    Place = "Long Island"
                 }
             };
 
+        /**
+         * GET Method usin Web API
+         */
         [HttpGet]
         public async Task<ActionResult<List<SuperHero>>> Get()
         {
+            return Ok(heroes);
+        }
+
+        /**
+         * GET Method usin Web API, find by Id
+         */
+        [HttpGet("{id}")]
+        public async Task<ActionResult<SuperHero>> Get(int id)
+        {
+            var hero = heroes.Find(x => x.Id == id);
+            if (hero == null)
+                return BadRequest("Hero not found!");
+            return Ok(hero);
+        }
+
+        /*
+         * POST Method usin Web API
+         */
+        [HttpPost]
+        public async Task<ActionResult<List<SuperHero>>> AddHero(SuperHero hero)
+        {
+            heroes.Add(hero);
+            return Ok(heroes);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<SuperHero>>> UpdateHero(SuperHero request)
+        {
+
+            var hero = heroes.Find(x => x.Id == request.Id);
+            if (hero == null)
+                return BadRequest("Hero not found!");
+
+            hero.Name = request.Name;
+            hero.FirstName = request.FirstName; 
+            hero.LastName = request.LastName;   
+            hero.Place = request.Place;
+
             return Ok(heroes);
         }
     }
